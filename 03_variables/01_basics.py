ffd6cd495f229ecb46903381c65e6387b5f5abc3
Pooja <141401125+poojaparab210@users.noreply.github.com>
2026-06-29 21:41:58 +0530

    Add files via upload

---
+++ 03_variables/01_basics.py
@@ -0,0 +1,11 @@
+# Variables are used to store data that can be used and manipulated in a program.
+# A variable is created when you assign to it using the operator.
+#example of variable assignment
+age =  34
+name = "Pooja"
+cgpa = 6.55
+# Rules:
+# 1. Variable names must start with a letter or an underscore.
+# 2. Variable names can only contain letters, numbers, and underscores.
+# 3.avoid python keywords as variable names for ex., print, if, else, etc.
+# 4. Variable names are case-sensitive.
---
+++ 03_variables/02_data_types.py
@@ -0,0 +1,20 @@
+# Data Types in Python:
+# 1. Integers(int): Whole nos. for example 1, -6   
+age = 3
+print(age)
+print(type(age))
+
+# 2. Floats(float): Decimal nos. for ex., 3.14 , 1.0
+cgpa = 8.2
+print(cgpa)
+print(type(cgpa))
+
+# 3. Strings(str): Text data enclosed in quotes for ex., "Hello", "Pooja" 
+name = "Pooja"
+print(name)
+print(type(name))
+
+# 4. Booleans(bool): Represents True or False
+is_completed = True
+print(is_completed)
+print(type(is_completed))
