1cc8581305c042ad4ed22643e1c1f36fc3a1b1ff
Pooja <141401125+poojaparab210@users.noreply.github.com>
2026-06-29 21:42:37 +0530

    Add files via upload

---
+++ 04_typecasting/01_typecasting.py
@@ -0,0 +1,34 @@
+# Typecasting is the process of converting one data type to another.
+# Python provides build-in functions for typecasting.
+# 1. int(): Converts to integers.
+# 2. float(): Converts to float.
+# 3. str(): Converts to string.
+# 4. bool(): Converts to boolean.
+a = 34
+b = "34"
+c = 34.6
+d = 223
+
+print(a)
+print(type(a))
+
+print(b)
+print(type(b))
+
+#Convert string(b) to an integer
+c = int(b)
+print(c)
+print(type(c))
+
+#Convert integer(d) to a string
+e = str(d)
+print(e)
+print(type(e))
+
+#Convert float(c) to a integer
+f = int(c)
+print(f)
+print(type(f))
+
+
+
---
+++ 04_typecasting/02_example.py
@@ -0,0 +1,17 @@
+# Convert string to integer
+num_str = "10"
+num_int = int(num_str)
+print(num_int)  # Output: 10
+print(type(num_int))  # Output: <class 'int'>
+
+# Convert integer to string
+num = 25
+num_str = str(num)
+print(num_str)  # Output: "25"
+print(type(num_str))  # Output: <class 'str'>
+
+# Convert float to integer
+num_float = 3.14
+num_int = int(num_float)    
+print(num_int)  # Output: 3
+print(type(num_int))  # Output: <class 'int'>
